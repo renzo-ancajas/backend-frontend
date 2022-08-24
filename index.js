ec233c41050a712adb5a86d9e5e6f90d4d8ece39
renzoancajas24 <108566575+renzo-ancajas@users.noreply.github.com>
2022-08-24 16:37:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,199 @@
+const http = require('http')
+const express = require('express')
+const { response } = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:  ', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(requestLogger)
+
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),'status code:',
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(
+    `
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${date}</p>
+    `
+  )
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  note ? response.json(note) : response.status(404).end()
+  console.log(note)
+  response.json(note)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  person ? response.json(person) : response.status(404).end()
+  console.log(person)
+  response.json(person)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id != id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = note.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+const generatePersonId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes/', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content : body.content,
+    important : body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+  notes = notes.concat(note)
+  console.log(note)
+  response.json(note)
+})
+
+app.post('/api/persons/', (request, response) => {
+  const body = request.body
+  const uniqueName = persons.find(person => person.name === body.name)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name & number missing' 
+    })
+  } else if (uniqueName) {
+    return response.status(400).json({
+      error: "name must be unique"
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generatePersonId()
+  }
+  persons = persons.concat(person)
+  console.log(person)
+  response.json(person)
+})
+
+
+//morgan.token('type', function (req, res) { return req.headers['content-type'] })
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.Port || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
