04673b0af8213cc2eacc73a1ca70e18c92fd4255
renzoancajas24 <108566575+renzo-ancajas@users.noreply.github.com>
2022-08-24 21:44:18 +0800

    port updated

--- index.js
+++ index.js
@@ -288,7 +288,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.Port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
